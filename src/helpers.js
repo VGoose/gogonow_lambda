11ed2cb122acf1799daaf287b009ca4111c92ae2
AnhVoMBP15 <anhvouw@gmail.com>
2018-09-09 08:11:20 -0400

    redefined functions to fit front end API

---
+++ src/helpers.js
@@ -0,0 +1,43 @@
+import stations from './stops.JSON';
+import { diff, findClosestIndex, getEuclideanDist } from './utils/helpers';
+
+async function getStations() {
+	let position = await getUserPosition();
+	let userLat = position.coords.latitude;
+	let userLon = position.coords.longitude;
+	let x, y, dist;
+
+	let _stations = [];
+	for (let key in stations) {
+		let stopLat = parseFloat(stations[key].stop_lat);
+		let stopLon = parseFloat(stations[key].stop_lon);
+		//constants are rough estimates of miles per degree of lat/lon for NYC
+		x = diff(userLat, stopLat) * 69.05;
+		y = diff(userLon, stopLon) * 52.35;
+		dist = getEuclideanDist(x, y); 
+		if(dist < 0.5 && _stations.length < 2
+		) {
+			stations[key].dist = dist;
+			_stations.push(stations[key])
+		}
+	}
+	_stations.sort((a, b) => {
+		return a.dist - b.dist;
+	})
+	return _stations
+}
+
+function getUserPosition() {
+	if (!navigator.geolocation) {
+		return 'Your browser does not support gelocation';
+	}
+	let geo = navigator.geolocation;
+	return new Promise((resolve, reject) => {
+		geo.getCurrentPosition(resolve, reject)
+	})
+}
+
+module.exports = {
+  getStations,
+	getUserPosition,
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,51 +1,60 @@
-import stops from '../stops.JSON';
 
+function findClosestIndex(x, arr, start, end) {
+	if(typeof x !== 'number') {
+		throw new Error('First argument must be a number.');
+	}
+	if(!Array.isArray(arr) || arr.length === 0) {
+		throw new Error('Second argument must be a non-empty, sorted array.');
+	}
+	if(typeof start !== 'number') {
+		start = null;
+	} 
+	if(typeof end !== 'number') {
+		end = null;
+	}
 
-// module.exports.findClosestStation = async function findClosestStation(x, radius) {
+	
+	let _end = end || arr.length;
+	let _start = start || 0;
+	var index = Math.floor((_end + _start) / 2)
+	let getVal = (obj) => {
+		return obj.eDist;
+	}
+	let value = getVal(arr[index])
 
-// 	let stationDists = await getStationsEuclideanDist();
-// 	let station;
+	if (value === x){
+		return index;
+	}
+	if((_end - _start) === 1) {
+		index = diff(getVal(arr[_start]), x) < diff(getVal(arr[_end]), x) ? 
+			_start : _end;
+		return index;
 
+	}
+	if(value < x) {
+		_start = index;
+		return findClosestIndex(x, arr, _start, _end);
+	}else if(value > x) {
+		_end = index;
+		return findClosestIndex(x, arr, _start, _end);
+	}
+}
+function diff(a, b) {
+	return Math.abs(a - b);
+}
 
-// 	//euclidean distance comparison
-// 	//calc euclidean distance of all stops, make into a sorted array
-// 	//match 
-// 	return station;
-// }
+function getEuclideanDist(a, b) {
+	let _a = parseFloat(a),
+		_b = parseFloat(b);
+	if(!_a || !b) {
+		throw new Error('Both arguments must be numbers');
+	}
 
+	let result = Math.sqrt(_a**2 + _b**2);
+	return result;
+}
 
 module.exports = {
-
-	findClosestStation: () => {
-
-	},
-
-	getUserPosition: () => {
-		if (!navigator.geolocation) {
-			return 'Your browser does not support gelocation';
-		}
-		let geo = navigator.geolocation;
-		return new Promise((resolve, reject) => {
-			geo.getCurrentPosition(resolve, reject)
-		})	
-	},
-
-	getEuclideanDist: (a, b) => {
-		if(typeof a && typeof b !== 'number') {
-			throw new Error('Both arguments must be numbers');
-		}
-		let result = Math.sqrt(a*a + b*b);
-		return result;
-	},
-
-	getStationsEuclideanDist: async () => {
-		let x, y;
-		let stationsEuclideanDist = [];
-		for(stop in stops) {
-			x = parseFloat(stops[stop].stop_lat);
-			y = parseFloat(stops[stop].stop_lon);
-			stationsEuclideanDist.push(getEuclideanDist(x, y));
-		}
-		return stationsEuclideanDist;
-	}
+	getEuclideanDist,
+	diff
 }
