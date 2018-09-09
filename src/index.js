4bc73824bf9e2216292fbae7ad5f14b45884942c
AnhVoMBP15 <anhvouw@gmail.com>
2018-09-09 11:20:14 -0400

    scaffolding

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryChart } from 'victory';
 
-import {getUserPosition} from './utils/helpers';
+import Transit from './components/transit';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      latitude: '',
-      longitude: '',
-      data: undefined,
-    }
   }
-  componentDidMount(){
-    navigator.geolocation ? 
-      this.loadPosition() : alert('Your browser does not have geolocation');
-  }
-  loadPosition = async function loadPosition() {
-    try {
-      let position = await getUserPosition();
-      let { latitude, longitude } = position.coords;
-      this.setState({ latitude, longitude  })
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   render() {
-    let { latitude, longitude} = this.state;
-    if(!latitude) {
-      return <p>Loading..</p>
-    }
     return(
-      <div>Lat: {latitude}, Long: {longitude}</div>
+      <Transit />
     )
-
   }
 }
 
