87fc97187f22022a7edb2530c9b235d18d8cf086
AnhVoMBP15 <anhvouw@gmail.com>
2019-01-30 18:09:03 -0500

    cleaned up files, add NODE_VERSION to toml

--- src/lambda/gogonow.js
+++ src/lambda/gogonow.js
@@ -2,19 +2,12 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const schedule = require('../../routes/api/schedule');
 const weather = require('../../routes/api/weather')
 
 const app = express();
 
-//cors setup
-var corsOptions = {
-  origin: ['http://localhost:8080', 'https://nervous-newton-e18e8b.netlify.com', 'http://localhost:3000', 'exp://127.0.0.1:19000'],
-  // credentials: true
-}
-app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
