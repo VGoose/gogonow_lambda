5725fd9788a7e91d8822b58571ee222814da7ccc
AnhVoMBP15 <anhvouw@gmail.com>
2019-01-29 22:08:44 -0500

    a;sdjfa;

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -14,22 +14,25 @@ router.get('/', (req, res) => {
     }
   }
 
-  getSchedules().then(
-    data => {
-      lastReqTime = Date.now()
-      backupData = data
-      res.status(200).send(JSON.stringify(data))
-      query = Schedule.findByIdAndUpdate('5bd75e39e819ae36c704f42a', { time: Date.now(), schedules: data }, { new: true, upsert: true });
-      query
-      .exec()
-        .then(document => {
-          backupData = document
-          res.status(200).send(JSON.stringify(document))
-        })
-        .catch(error => console.log(error) || res.status(404).send(error))
-    },
-    error => console.log('schedule error ' + error) || res.status(404).send(error)
-  )
+  getSchedules()
+    .then(
+      data => {
+        console.log('get schedules finished')
+        lastReqTime = Date.now()
+        backupData = data
+        res.status(200).send(JSON.stringify(data))
+
+        // query = Schedule.findByIdAndUpdate('5bd75e39e819ae36c704f42a', { time: Date.now(), schedules: data }, { new: true, upsert: true });
+        // query
+        //   .exec()
+        //   .then(document => {
+        //     backupData = document
+        //     res.status(200).send(JSON.stringify(document))
+        //   })
+        //   .catch(error => console.log(error) || res.status(404).send(error))
+      },
+      error => console.log('schedule error ' + error) || res.status(404).send(error)
+    )
 })
 
 
